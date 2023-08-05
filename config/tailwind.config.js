a3ddee782797e636451757a9bb9e54cf6f0eba10
thekuen <thekuen@gamil.com>
2023-08-05 17:33:35 +0800

    build: install Preline and g controller homepages

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {
@@ -19,5 +20,6 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
+    require('preline/plugin'),
   ]
 }
