ea606fac48f8041140fc50642e30c377979f7620
thekuen <thekuen@gamil.com>
2023-08-06 13:41:35 +0800

    style: homepage layout 0806

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'homepages#index'
+  get '/node_modules/preline/dist/preline.js', to: 'homepages#index'
+
+  get '/blogs', to: 'blogs#index'
+  get '/blogs/new', to: 'blogs#new', as: 'new_blog'
+  post '/blogs'   , to: 'blogs#create'
+  get '/blogs/:id', to: 'blogs#show', as: 'blog'
+  get '/blogs/:id/edit', to: 'blogs#edit' , as: 'edit_blog'
+  patch '/blogs/:id', to: 'blogs#update'
+
+
+
+
 end
