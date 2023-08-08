79ad188a2037310c0747c7049b88fd75db9482b6
Zinni Chang <czinni853@gmail.com>
2023-08-08 19:44:14 +0800

    the first version

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_035026) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_134439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.text "summary"
+    t.text "education"
+    t.text "work_experience"
+    t.text "skills"
+    t.text "languages"
+    t.text "certifications"
+    t.text "projects"
+    t.text "social_links"
+    t.string "avatar"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_035026) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
