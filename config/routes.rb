8a51783904db86e121f53fa101a4ba8650b0f266
Zinni Chang <czinni853@gmail.com>
2023-08-11 00:37:17 +0800

    [build]: the profile CRUD and add the css of all views

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :profiles, except: [:destroy]
+  # scope path: '@:account' do
+    resource :profile, except: [:destroy]
+  # end
 end
