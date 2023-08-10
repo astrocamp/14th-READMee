d432054c4a3fc9d7a56b17c784e1704b23f89d26
Zinni Chang <czinni853@gmail.com>
2023-08-11 00:37:17 +0800

    [build]: the profile CRUD and add the css of all views

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,12 @@
 class Profile < ApplicationRecord
-  belongs_to :user
+  validates :full_name, :phone, :address, :education, :about_me, :work_experience, presence: true
+  
+  # relationships
+  # belongs_to :user
+  has_one_attached :avatar
+
+  def add_language(language, level)
+    self.languages ||= {}
+    self.languages[language] = level
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 Rails.application.routes.draw do
   root 'pages#home'
 
@@ -6,3 +7,14 @@ Rails.application.routes.draw do
   resources :profiles, except: [:destroy]  
   resources :resumes
 end
+=======
+Rails.application.routes.draw do
+  root 'pages#home'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  # scope path: '@:account' do
+    resource :profile, except: [:destroy]
+  # end
+end
+>>>>>>> 8a51783 ([build]: the profile CRUD and add the css of all views)
