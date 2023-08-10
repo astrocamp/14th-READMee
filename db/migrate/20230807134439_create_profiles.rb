049dc67a2aed959e0f942327e24550a5f5262843
Zinni Chang <czinni853@gmail.com>
2023-08-11 00:34:49 +0800

    [build]: add active storage and profile migration

--- db/migrate/20230807134439_create_profiles.rb
+++ db/migrate/20230807134439_create_profiles.rb
@@ -1,15 +1,20 @@
 class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
     create_table :profiles do |t|
-      t.string :education
-      t.string :languages
-      t.string :skills
-      t.text :summary
-      t.text :work_experience
-      t.string :certifications
-      t.string :projects
-      t.string :social_links
       t.string :avatar
+      t.string :full_name
+      t.string :phone
+      t.string :address
+      t.string :job_title
+      t.string :education
+      t.jsonb :languages, default: {}
+      t.text :about_me
+      t.text :work_experience
+      t.string :projects
+      t.string :linkedin
+      t.string :facebook
+      t.string :github
+      t.string :website
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
