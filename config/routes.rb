813ef01b5a421a0db983d8d5289ec9b41bdd457e
thekuen <thekuen@gamil.com>
2023-08-10 14:34:06 +0800

    doc: remove controller homepages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'homepages#index'
+  
   get '/node_modules/preline/dist/preline.js', to: 'homepages#index'
 
   get '/blogs', to: 'blogs#index'
