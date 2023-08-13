2b08ec356a0fe6fbfeaeb919112e2f73037a5e1f
DOJO <kevin11272@gmail.com>
2023-08-13 11:46:25 +0800

    add resume model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_025852) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_032450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_025852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["deleted_at"], name: "index_blogs_on_deleted_at"
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.integer "block"
+    t.string "avatar"
+    t.text "basic_info"
+    t.text "social_links"
+    t.text "summary"
+    t.text "work_experience"
+    t.string "skills"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
