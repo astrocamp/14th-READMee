763727279b750683849d403e903833dfd1f22c60
DOJO <kevin11272@gmail.com>
2023-08-14 12:10:15 +0800

    resume_system

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_032450) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
+    t.string "confirmation_token" 
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
