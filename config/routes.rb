3846c527499a3543c58e8f1e387c0a3189225310
Zinni Chang <czinni853@gmail.com>
2023-08-14 16:46:15 +0800

    [fix]: revised routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
 Rails.application.routes.draw do
   root 'pages#home'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :profiles, except: [:destroy]  
+  scope path: '@:account' do
+  resource :profile, except: [:destroy]
+  end 
+  
   resources :resumes
 end
-=======
-Rails.application.routes.draw do
-  root 'pages#home'
-
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
-  # scope path: '@:account' do
-    resource :profile, except: [:destroy]
-  # end
-end
->>>>>>> 8a51783 ([build]: the profile CRUD and add the css of all views)
