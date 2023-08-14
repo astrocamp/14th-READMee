aa51e875deb530e62a90fdd4dfea5ef98b35916c
komaki <zhfa13z@gmail.com>
2023-08-14 12:43:25 +0800

    [build]:merge dev

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_035026) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_070119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone"
+    t.text "about"
+    t.integer "population"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "num_of_people"
+    t.string "seniority"
+    t.integer "salary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.bigint "company_id"
+    t.index ["company_id"], name: "index_jobs_on_company_id"
+    t.index ["deleted_at"], name: "index_jobs_on_deleted_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
