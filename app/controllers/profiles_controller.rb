659d6c6cb93fcc78eab3286b82ce12de990c6a90
Zinni Chang <czinni853@gmail.com>
2023-08-15 20:51:04 +0800

    [fix]: revised the association between user and profile and rebuild css style

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,19 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_profile, only: [:show, :edit, :update]
 
   def show
   end
 
   def new
-    @profile = Profile.new
+    @profile = current_user.build_profile
   end
 
   def create
-    @profile = Profile.new(profile_params)
+    @profile = current_user.build_profile(profile_params)
+    
     if @profile.save
-      redirect_to profile_path, notice: "Profile was successfully saved!"
+      redirect_to profile_path(@profile), notice: "恭喜完成第一步！建立個人檔案成功！"
     else
       render :new
     end
@@ -22,19 +24,18 @@ class ProfilesController < ApplicationController
 
   def update
     if @profile.update(profile_params)
-      redirect_to profile_path, notice: 'Profile was successfully updated.'
+      redirect_to profile_path, notice: "更新個人檔案成功！"
     else
       render :edit
     end
   end
 
   private
-  
   def set_profile
-    @profile = current_user
+    @profile = current_user.profile
   end
 
   def profile_params
-    params.require(:profile).permit(:avatar, :full_name, :phone, :address, :education, :about_me, :work_experience, :projects, :linkedin, :facebook, :github, :website, languages: {})
+    params.require(:profile).permit(:avatar, :full_name, :phone, :address, :job_title, :education, :about_me, :work_experience, :projects, :linkedin, :facebook, :github, :website, languages: {})
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,8 @@
 class Profile < ApplicationRecord
-  validates :full_name, :phone, :address, :education, :about_me, :work_experience, presence: true
+  validates :full_name, :phone, :address, :job_title, :education, :about_me, :work_experience, presence: true
   
   # relationships
-  # belongs_to :user
+  belongs_to :user
   has_one_attached :avatar
 
   def add_language(language, level)
