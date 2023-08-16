0f36da5adea5142132280ce37afde10dcf127031
DOJO <kevin11272@gmail.com>
2023-08-16 13:38:11 +0800

    [feat] signin_path_to_profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,16 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   include Pundit::Authorization
+  # 避免 CSRF 攻擊
+  protect_from_forgery with: :exception
 
   # 設定語言環境為繁體中文
   def set_locale
     I18n.locale = :'zh-TW'
+  end
+  
+  # 登入之後跳轉的路徑
+  def after_sign_in_path_for(resource)
+    user_profile_path(account: current_user.account)
   end
 end
