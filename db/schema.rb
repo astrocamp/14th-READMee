c44da25d8b884169ea105272734fb4febbbb5f20
DOJO <kevin11272@gmail.com>
2023-08-18 17:37:58 +0800

    [fix]: signin_path_to_profile

--- db/schema.rb
+++ db/schema.rb
@@ -1,14 +1,18 @@
-ActiveRecord::Schema[7.0].define(version: 20_230_813_032_450) do
-  enable_extension "plpgsql"
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "deleted_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["deleted_at"], name: "index_blogs_on_deleted_at"
-  end
+ActiveRecord::Schema[7.0].define(version: 2023_08_14_095140) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "profiles", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -16,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 20_230_813_032_450) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
+
   create_table "resumes", force: :cascade do |t|
     t.integer "block"
     t.string "avatar"
