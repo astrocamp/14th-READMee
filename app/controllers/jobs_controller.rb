963728c9d083d4b5a99622f9c38d7710e8efbf9e
DOJO <kevin11272@gmail.com>
2023-08-21 19:50:29 +0800

    [fix]: pr

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,7 +12,6 @@ class JobsController < ApplicationController
   end
 
   def show
-  
   end
 
   def create
@@ -25,7 +24,6 @@ class JobsController < ApplicationController
   end
 
   def edit
-  
   end
 
   def update
