833afb7a4785385ab0fa33ba9574e9203b9abbcb
DOJO <kevin11272@gmail.com>
2023-08-24 18:06:04 +0800

    [fix]: rebase_to_dev

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def toggle
-      if current_user.employer?
-        current_user.update(role: 'job_seeker')
-      elsif current_user.job_seeker?
-        current_user.update(role: 'employer')
-      end
-      redirect_to root_path, notice: "角色已切换"
+    if current_user.employer?
+      current_user.update(role: 'job_seeker')
+    elsif current_user.job_seeker?
+      current_user.update(role: 'employer')
     end
+    redirect_to root_path, notice: "角色已切换"
+  end
 end
