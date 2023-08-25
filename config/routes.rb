f6a2d64fe3b8fddc0dd2aaf1ab930c0646c597de
thekuen <thekuen@gamil.com>
2023-08-25 18:28:28 +0800

    [fix]: remove blog

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   resources :articles
 
-  resources :blogs 
-
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :resumes, only:[:new]
  
