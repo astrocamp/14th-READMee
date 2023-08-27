401cd50a92111c0b360d1fbb23a9fd9eb2e73b7e
komaki <zhfa13z@gmail.com>
2023-08-27 21:17:52 +0800

    [fix]: remove job.rb company_id

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,7 +4,7 @@ class Job < ApplicationRecord
   belongs_to :company
 
   def self.ransackable_attributes(auth_object = nil)
-    [ "company_id", "content", "title"]
+    ["content", "title"]
   end
 
   def self.ransackable_associations(auth_object = nil)
