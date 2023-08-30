c1dab484930c7312cd609c0a6fabf71c5c89399d
thekuen <thekuen@gamil.com>
2023-08-30 20:08:04 +0800

    [fix]: PR conflict

--- db/migrate/20230830035043_create_job_matchings.rb
+++ db/migrate/20230830035043_create_job_matchings.rb
@@ -3,7 +3,7 @@ class CreateJobMatchings < ActiveRecord::Migration[7.0]
     create_table :job_matchings do |t|
       t.references :user, null: false, foreign_key: true
       t.references :job, null: false, foreign_key: true
-
+      t.references :company, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_105606) do
     t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
+  create_table "job_matchings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "job_id", null: false
+    t.bigint "company_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_job_matchings_on_company_id"
+    t.index ["job_id"], name: "index_job_matchings_on_job_id"
+    t.index ["user_id"], name: "index_job_matchings_on_user_id"
+  end
+
   create_table "jobs", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -137,7 +148,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_105606) do
     t.text "education"
     t.string "project_title"
     t.string "education_title"
+<<<<<<< HEAD
     t.boolean "publish"
+=======
+>>>>>>> [build]: migration job_matching
     t.index ["component_name"], name: "index_resumes_on_component_name", unique: true
     t.index ["user_id"], name: "index_resumes_on_user_id"
   end
@@ -172,6 +186,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_105606) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "articles", "users"
   add_foreign_key "companies", "users"
+  add_foreign_key "job_matchings", "companies"
+  add_foreign_key "job_matchings", "jobs"
+  add_foreign_key "job_matchings", "users"
   add_foreign_key "profile_skills", "profiles"
   add_foreign_key "profile_skills", "skills"
   add_foreign_key "profiles", "users"
