429b87dcf4bfa9ef55a9ae9edba1adf8aaebaf51
DOJO <kevin11272@gmail.com>
2023-08-31 07:48:57 +0800

    [fix]: write_process_of_profile

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -5,35 +5,7 @@ class ResumesController < ApplicationController
   before_action :find_resume, only: [:edit, :update, :second]
 
   def edit
-    if @resume.basic_info.present?
-      @formatted_basic_info = sanitize(@resume.basic_info).gsub("\n", '<br>').html_safe
-    else
-      @formatted_basic_info = ""
-    end
-
-    if @resume.social_links.present?
-      @formatted_social_links = sanitize(@resume.social_links).gsub("\n", '<br>').html_safe
-    else
-      @formatted_social_links = ""
-    end
-
-    if @resume.work_experience.present?
-      @formatted_work_experience = sanitize(@resume.work_experience).gsub("\n", '<br>').html_safe
-    else
-      @formatted_work_experience = ""
-    end
-
-    if JSON.parse(@resume.skills) == [""]
-      ['請寫入內容']
-    else
-      @skills = JSON.parse(@resume.skills)
-    end
-
-    if JSON.parse(@resume.languages) == [""]
-      ['請寫入內容']
-    else
-      @languages = JSON.parse(@resume.languages)
-    end
+    @profile = Profile.find_by(current_user.id)
   end
 
   def update
