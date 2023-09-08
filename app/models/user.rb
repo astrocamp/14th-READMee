ff8dab0f4eb5dda1414daa56ccc170931ce333bc
Zinni Chang <czinni853@gmail.com>
2023-09-08 16:37:05 +0800

    [fix]: mailgun confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   before_save :set_account
 
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_one :profile
   has_one :company
