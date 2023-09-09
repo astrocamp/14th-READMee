2c50714c41842722405f9ff976d6310ab804c729
thekuen <thekuen@gamil.com>
2023-09-09 15:49:23 +0800

    [fix]: fix resume controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def link_based_on_role(user)
     if user.admin?
-      link_to '管理後臺', admin_users_path(account: user.account),class: 'hover:text-amber-600 mx-1'
+      link_to '管理後臺', admin_users_path(account: user.account), class: 'hover:text-amber-600 mx-1'
     elsif user.role == 'job_seeker'
       link_to '個人檔案', dashboard_path(account: user.account)
     elsif user.role == 'employer'
