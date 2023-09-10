e8ee48ce22f97985bc40220c161bbc0bd33bdb2d
q2379685 <q2379685@gmail.com>
2023-09-10 17:46:38 +0800

    固定欄位加組件

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -27,16 +27,19 @@ class ResumesController < ApplicationController
   end
 
   def update
-    if params[:published] == "發佈" && params[:publish] == "false"
-      @resume.update(publish: true)
-      redirect_to resumes_path
-    elsif params[:save] == "更新"
+    # if params[:published] == "發佈" && params[:publish] == "false"
+    #   p "*" * 100
+    #   p '發佈'
+    #   @resume.update(publish: true)
+    #   redirect_to resumes_path
+    # elsif params[:save] == "更新"
+    #   p "*" * 100
+    #   p '更新'
       if @resume.update(resume_params)
-        redirect_to edit_resume_path(account: current_user.account, id: @resume)
       else
         render :edit
       end
-    end
+    # end
   end
 
   def find_resume
@@ -44,6 +47,6 @@ class ResumesController < ApplicationController
   end
 
   def resume_params
-    params.require(:resume).permit(:id, :social_links, :skills, :user_id, :about_me, :about_me_title, :work_experience_title, :languages, :project_title, :education_title, :publish, :full_name, :phone, :address, :job_hunting, :work_exp_name_1, :work_exp_content_1, :work_exp_start_date_1, :work_exp_end_date_1, :work_exp_name_2, :work_exp_content_2, :work_exp_start_date_2, :work_exp_end_date_2, :project_name_1, :project_name_2, :project_content_1, :project_content_2, :education_name_1, :education_start_date_1, :education_end_date_1, :education_name_2, :education_start_date_2, :education_end_date_2)
+    params.require(:resume).permit(:id, :social_links, :skills, :user_id, :about_me, :about_me_title, :work_experience_title, :languages, :project_title, :education_title, :publish, :full_name, :phone, :address, :job_hunting, :work_exp_name_1, :work_exp_content_1, :work_exp_start_date_1, :work_exp_end_date_1, :work_exp_name_2, :work_exp_content_2, :work_exp_start_date_2, :work_exp_end_date_2, :project_name_1, :project_name_2, :project_content_1, :project_content_2, :education_name_1, :education_start_date_1, :education_end_date_1, :education_name_2, :education_start_date_2, :education_end_date_2, :area_1, :area_2, :area_3, :area_4)
   end
 end
