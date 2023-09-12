7511d1c9d694ed8e520550b2e8f1b059d5c0fd34
thekuen <thekuen@gamil.com>
2023-09-12 13:21:00 +0800

    [style]: layout

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -12,7 +12,7 @@ class ResumesController < ApplicationController
   end
 
   def show
-    @resume = Resume.find
+    @resume = Resume.find(params[:id])
   end
 
   def edit
@@ -34,7 +34,6 @@ class ResumesController < ApplicationController
   def resume_params
     params.require(:resume).permit(:social_links, :about_me, :languages, :publish, :full_name, :phone, :address, :job_hunting, :work_exp_name_1, :work_exp_content_1, :work_exp_start_date_1, :work_exp_end_date_1, :work_exp_name_2, :work_exp_content_2, :work_exp_start_date_2, :work_exp_end_date_2, :project_name_1, :project_name_2, :project_content_1, :project_content_2, :education_name_1, :education_start_date_1, :education_end_date_1, :education_name_2, :education_start_date_2, :education_end_date_2, :area_1, :area_2, :area_3, :area_4)
   end
-  
-  
+
+
 end
- 
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    'node_modules/preline/dist/*.js',
+
   ],
   theme: {
     extend: {
@@ -21,6 +21,6 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-    require('preline/plugin'),
+
   ],
 };
