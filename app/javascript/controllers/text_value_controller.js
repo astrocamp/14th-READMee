86bf02ee0f5f4af44506c775d7d4d8759f95dca8
DOJO <kevin11272@gmail.com>
2023-09-12 18:51:21 +0800

    [fix]: run_rubocop

--- app/javascript/controllers/text_value_controller.js
+++ app/javascript/controllers/text_value_controller.js
@@ -1,6 +1,5 @@
 import { Controller } from '@hotwired/stimulus';
 
-// Connects to data-controller="text-value"
 export default class extends Controller {
   static targets = ['input'];
   clean() {
