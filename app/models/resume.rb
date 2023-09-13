91b3747f33fd7edf1e54ed85fa3d3280d0fa7327
thekuen <thekuen@gamil.com>
2023-09-13 16:42:07 +0800

    [style]: index card

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -4,9 +4,9 @@ class Resume < ApplicationRecord
   has_many :resume_skills
   has_many :skills, through: :resume_skills
 
-  validates :about_me, length: { maximum: 370 }
+  validates :about_me, length: { maximum: 540 }
   validates :work_exp_content_1, length: { maximum: 120 }
   validates :work_exp_content_2, length: { maximum: 120 }
-  validates :project_content_1, length: { maximum: 188 }
-  validates :project_content_2, length: { maximum: 188 }
+  validates :project_content_1, length: { maximum: 210 }
+  validates :project_content_2, length: { maximum: 210 }
 end
