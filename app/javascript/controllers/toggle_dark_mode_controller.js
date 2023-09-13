76cb5d8a84462a5167a61efed30686dfe05c0241
DOJO <kevin11272@gmail.com>
2023-09-13 13:13:52 +0800

    [fix]: remove_console_log

--- app/javascript/controllers/toggle_dark_mode_controller.js
+++ app/javascript/controllers/toggle_dark_mode_controller.js
@@ -5,9 +5,9 @@ export default class extends Controller {
   darkModeEnabled = false;
 
   connect() {
-    const savedDarkMode = localStorage.getItem('darkModeEnabled');
-    if (savedDarkMode) {
-      this.darkModeEnabled = savedDarkMode === 'true';
+    const savedDarkMode = localStorage.getItem('darkMode');
+    if (savedDarkMode === 'true') {
+      this.darkModeEnabled = true;
       this.applyDarkMode();
     }
   }
@@ -15,18 +15,15 @@ export default class extends Controller {
   toggleDarkMode() {
     this.darkModeEnabled = !this.darkModeEnabled;
     this.applyDarkMode();
-    localStorage.setItem('darkModeEnabled', this.darkModeEnabled);
+    localStorage.setItem('darkMode', this.darkModeEnabled);
   }
 
   applyDarkMode() {
-    console.log('set dark');
     const htmlTag = this.htmlTarget;
     if (this.darkModeEnabled) {
-      htmlTag.classList.remove('dark');
-      htmlTag.classList.add('light');
-    } else {
       htmlTag.classList.add('dark');
-      htmlTag.classList.remove('light');
+    } else {
+      htmlTag.classList.remove('dark');
     }
   }
 }
