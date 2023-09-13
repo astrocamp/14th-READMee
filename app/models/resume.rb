de7e9827f044068cbf2902d8505e97c417317f16
q2379685 <q2379685@gmail.com>
2023-09-13 10:57:28 +0800

    [fix]: resume word limit

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -3,4 +3,10 @@ class Resume < ApplicationRecord
   belongs_to :profile
   has_many :resume_skills
   has_many :skills, through: :resume_skills
+
+  validates :about_me, length: { maximum: 370 }
+  validates :work_exp_content_1, length: { maximum: 120 }
+  validates :work_exp_content_2, length: { maximum: 120 }
+  validates :project_content_1, length: { maximum: 188 }
+  validates :project_content_2, length: { maximum: 188 }
 end
